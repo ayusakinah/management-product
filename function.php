45014ce04c24d02043c78d46775f6f00b64b6719
Ayu Dariah Sakinah <ayudariahs@gmail.com>
2025-01-22 10:33:57 +0700

    add product to database

--- function.php
+++ function.php
@@ -4,4 +4,18 @@ session_start();
 // Membuat koneksi ke database
 $conn = mysqli_connect("localhost","root","","db_management_product");
 
+// Tambah produk
+if(isset($_POST['addproduct'])) {
+    $productname = $_POST['name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query($conn, "insert into product (product_name, deskripsi, price, stock) values('$productname', '$description', '$price', '$stock')");
+
+    if($addtotable) {
+        header('location:index.php');
+    }
+}
+
 ?>
